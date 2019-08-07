9629bd4aaf73f10a27a6282e6f70b0c798b1f00c
obarra <barra_omar@hotmail.com>
2019-08-07 00:40:00 -0300

    method for sort

---
+++ src/main/java/com/obarra/littletaste/algorithm/Sorting.java
@@ -0,0 +1,16 @@
+package com.obarra.littletaste.algorithm;
+
+public class Sorting {
+
+    public void quickSortOnePivot(int[] elements){
+
+    }
+
+    public void quickSortDualPivot(int[] elements){
+
+    }
+
+    public void mergeSort(int[] elements){
+
+    }
+}
