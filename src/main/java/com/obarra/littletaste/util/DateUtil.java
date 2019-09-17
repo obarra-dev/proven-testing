22f8bb95910a4cd4b05d8b1b3314b0b06e189fbe
obarra <barra_omar@hotmail.com>
2019-09-16 23:26:53 -0300

    date with out time

---
+++ src/main/java/com/obarra/littletaste/util/DateUtil.java
@@ -0,0 +1,30 @@
+package com.obarra.littletaste.util;
+
+import java.util.Calendar;
+import java.util.Date;
+
+public class DateUtil {
+
+    /**
+     * This method returns a full current date with the time set to zero,
+     * but we can't ignore the time. Use Calendar
+     */
+    public static Date getDateWithoutTime(){
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(Calendar.HOUR_OF_DAY, 0);
+        calendar.set(Calendar.MINUTE, 0);
+        calendar.set(Calendar.SECOND, 0);
+        calendar.set(Calendar.MILLISECOND, 0);
+        return calendar.getTime();
+    }
+
+    public static Date getDateWithoutTime(final Date date){
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(date);
+        calendar.set(Calendar.HOUR_OF_DAY, 0);
+        calendar.set(Calendar.MINUTE, 0);
+        calendar.set(Calendar.SECOND, 0);
+        calendar.set(Calendar.MILLISECOND, 0);
+        return calendar.getTime();
+    }
+}
