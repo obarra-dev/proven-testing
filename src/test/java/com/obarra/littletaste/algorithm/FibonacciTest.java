05becb1a48c1aca5b452c59c20776217a4cec7d5
obarra <barra_omar@hotmail.com>
2020-02-08 23:30:20 -0300

    add asserts for fibonacci

--- src/test/java/com/obarra/littletaste/algorithm/FibonacciTest.java
+++ src/test/java/com/obarra/littletaste/algorithm/FibonacciTest.java
@@ -1,18 +1,33 @@
 package com.obarra.littletaste.algorithm;
 
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
 
 import static org.junit.Assert.*;
 
 public class FibonacciTest {
 
+    private Integer seed;
+
+    private List<Integer> expected;
+
+    @Before
+    public void setUp() {
+        seed = 10;
+        expected = Arrays.asList(0, 1, 1, 2, 3, 5, 8, 13, 21, 34);
+    }
+
     @Test
     public void generateByStream() {
-        System.out.println(Fibonacci.generate(10));
+        Assert.assertEquals(expected, Fibonacci.generate(10));
     }
 
     @Test
     public void generate() {
-        System.out.println(Fibonacci.generateByStream(10));
+        Assert.assertEquals(expected, Fibonacci.generateByStream(10));
     }
 }
