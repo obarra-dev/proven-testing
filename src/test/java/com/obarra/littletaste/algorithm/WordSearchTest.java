55924a14c198adb6d717dfaf8e59e23cca3a1335
Omar Barra <obarra@sysone.com>
2020-02-13 21:04:55 -0300

    local date Util

--- src/test/java/com/obarra/littletaste/algorithm/WordSearchTest.java
+++ src/test/java/com/obarra/littletaste/algorithm/WordSearchTest.java
@@ -8,31 +8,34 @@ public class WordSearchTest {
 
     @Test
     public void getIndexesOfMatchedWords() {
+    	int d = "dssdrgttfvttuggggkiolo".indexOf("gggg");
+    	System.out.println(d);
+    	
         char[][] m = {{'o','m','a'},{'o','m','a'},{'o','m','a'}};
         WordSearch.getIndexesOfMatchedWord(m, "oma");
     }
-    @Test
-    public void getIndexesOfMatchedWordsd() {
-        char[][] m = {{'o','m','a','r'},{'o','m','a','r'},{'o','m','a','r'},{'o','m','a','r'}};
-        WordSearch.getIndexesOfMatchedWord(m, "ramo");
-    }
-
-    @Test
-    public void getIndexesOfMatchedWordStrategyCheckCellsAndEightDirections() {
-        char matrix[][] = {{'o','m','a'},{'o','m','a'},{'o','m','a'}};;
-        WordSearch.getIndexesStrategyCheckCellsAndEightDirections(matrix, "oma");
-    }
-    @Test
-    public void getIndexesOfMatchedWordStrategyCheckCellsAndEightDirectionsDER() {
-        char matrix[][] = {{'o','m','a','r'},{'o','m','a','d'},{'o','m','a','f'}};;
-        WordSearch.getIndexesStrategyCheckCellsAndEightDirections(matrix, "omar");
-    }
-
-    @Test
-    public void getIndexesOfMatchedWordStrategyCheckCellsAndEightDirectionsDERf() {
-        char matrix[][] = {{'o','m','a','r'},{'o','m','a','d'},{'o','m','a','f'}};;
-        WordSearch.getIndexesStrategyCheckCellsAndEightDirections(matrix, "aaa");
-    }
+//    @Test
+//    public void getIndexesOfMatchedWordsd() {
+//        char[][] m = {{'o','m','a','r'},{'o','m','a','r'},{'o','m','a','r'},{'o','m','a','r'}};
+//        WordSearch.getIndexesOfMatchedWord(m, "ramo");
+//    }
+//
+//    @Test
+//    public void getIndexesOfMatchedWordStrategyCheckCellsAndEightDirections() {
+//        char matrix[][] = {{'o','m','a'},{'o','m','a'},{'o','m','a'}};;
+//        WordSearch.getIndexesStrategyCheckCellsAndEightDirections(matrix, "oma");
+//    }
+//    @Test
+//    public void getIndexesOfMatchedWordStrategyCheckCellsAndEightDirectionsDER() {
+//        char matrix[][] = {{'o','m','a','r'},{'o','m','a','d'},{'o','m','a','f'}};;
+//        WordSearch.getIndexesStrategyCheckCellsAndEightDirections(matrix, "omar");
+//    }
+//
+//    @Test
+//    public void getIndexesOfMatchedWordStrategyCheckCellsAndEightDirectionsDERf() {
+//        char matrix[][] = {{'o','m','a','r'},{'o','m','a','d'},{'o','m','a','f'}};;
+//        WordSearch.getIndexesStrategyCheckCellsAndEightDirections(matrix, "aaa");
+//    }
 
 
 }
