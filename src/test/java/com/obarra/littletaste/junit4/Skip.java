31bba5400fbdba7852254c816edd93d2f43fc8a0
obarra <barra_omar@hotmail.com>
2020-02-23 16:32:54 -0300

    return probe to junit4, with vintage

---
+++ src/test/java/com/obarra/littletaste/junit4/Skip.java
@@ -0,0 +1,13 @@
+package com.obarra.littletaste.junit4;
+
+import org.junit.Assume;
+
+public class Skip {
+    public static void IF(Boolean condition) {
+        Assume.assumeFalse(condition);
+    }
+
+    public static void UNLESS(Boolean condition) {
+        Assume.assumeTrue(condition);
+    }
+}
---
+++ src/test/java/com/obarra/littletaste/junit4/SuitTestTest.java
@@ -0,0 +1,12 @@
+package com.obarra.littletaste.junit4;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(value = Suite.class)
+@Suite.SuiteClasses({
+    BeforeAfterTest.class,
+    ParameterizedTest.class
+})
+public class SuitTestTest {
+}
